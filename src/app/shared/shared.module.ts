f420032d603d467780830ad4f1c02148d002e144
Johannes Trepesch <web-git@johannestrepesch.de>
2019-06-16 14:38:14 +0200

    refactor: extract component for product details

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AddressFormComponent } from './components/address-form/address-form.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
-  declarations: [AddressFormComponent],
+  declarations: [AddressFormComponent, ProductComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [AddressFormComponent]
+  exports: [AddressFormComponent, ProductComponent]
 })
 export class SharedModule {}
